a3bae6a570f1b41103bf8f77593abab87d0aa3e2
MunarbekJ <munarbek-z@mail.ru>
2023-04-11 12:05:20 +0600

    .

---
+++ blog/post/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Post
+
+
+def posts_list(request):
+    queryset = Post.objects.all()
+    return render(request, 'listing.html', {'posts':queryset})
+
